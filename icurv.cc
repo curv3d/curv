19aadabee28e35e5393c4149e8c14fbdd4f538f3
Doug Moen <doug@moens.org>
2016-05-13 10:49:44 -0400

    use GNU readline library

--- icurv.cc
+++ icurv.cc
@@ -1,3 +1,7 @@
+extern "C" {
+#include <readline/readline.h>
+#include <string.h>
+}
 #include <iostream>
 #include <curv/parse.h>
 #include <curv/eval.h>
@@ -5,16 +9,16 @@
 int
 main(int, char**)
 {
-    std::string line;
     for (;;) {
-        std::cout << "curv> ";
-        if (!std::getline(std::cin, line)) {
+        char* line = readline("curv> ");
+        if (line == nullptr) {
             std::cout << "\n";
             return 0;
         }
-        curv::Script script("<stdin>", line.data(), line.data() + line.size());
+        curv::Script script("<stdin>", line, line + strlen(line));
         auto expr = curv::parse(script);
         double val = curv::eval(*expr);
         std::cout << val << "\n";
+        free(line);
     }
 }
