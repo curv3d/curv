782817e8b7a84f5ff9ccf4eec5da6ab1e8ee85cb
Doug Moen <doug@moens.org>
2018-06-03 17:58:49 -0400

    better errors for `match`

--- libcurv/builtin.cc
+++ libcurv/builtin.cc
@@ -615,7 +615,10 @@ struct Match_Function : public Legacy_Function
         std::vector<Shared<Function>> cases;
         for (size_t i = 0; i < list->size(); ++i)
             cases.push_back(list->at(i).to<Function>(At_Index(i,ctx0)));
-        return {make<Piecewise_Function>(cases)};
+        auto mf = make<Piecewise_Function>(cases);
+        mf->name_ = name_;
+        mf->argpos_ = 1;
+        return {mf};
     }
 };
 
