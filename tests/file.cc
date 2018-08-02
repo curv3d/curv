a33371aabab97469678d4e6742825542133306f3
Doug Moen <doug@moens.org>
2018-08-01 22:29:19 -0400

    image export: class Output_File: unit tests

---
+++ tests/file.cc
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+#include <libcurv/output_file.h>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+
+using namespace std;
+using namespace curv;
+namespace fs = Filesystem;
+
+void writefile(const fs::path& path, const std::string& data)
+{
+    ofstream o(path.c_str());
+    o << data;
+    o.close();
+}
+std::string readfile(const fs::path& path)
+{
+    ifstream i(path.c_str());
+    stringstream s;
+    s << i.rdbuf();
+    return s.str();
+}
+
+TEST(curv, output_file)
+{
+    Output_File f1;
+    stringstream b1;
+    f1.set_ostream(&b1);
+    f1.open();
+    f1.ostream() << "foo";
+    ASSERT_EQ(b1.str(), "");
+    f1.commit();
+    ASSERT_EQ(b1.str(), "foo");
+
+    Output_File f2;
+    stringstream b2;
+    f2.set_ostream(&b2);
+    writefile(f2.path(), "foo");
+    ASSERT_EQ(b2.str(), "");
+    f2.commit();
+    ASSERT_EQ(b2.str(), "foo");
+
+    Output_File f3;
+    fs::path p3(",f3");
+    f3.set_path(p3);
+    f3.open();
+    f3.ostream() << "foo";
+    ASSERT_EQ(readfile(p3), "");
+    f3.commit();
+    ASSERT_EQ(readfile(p3), "foo");
+    remove(",f3");
+
+    Output_File f4;
+    fs::path p4(",f4");
+    f4.set_path(p4);
+    writefile(f4.path(), "foo");
+    ASSERT_EQ(readfile(p4), "");
+    f4.commit();
+    ASSERT_EQ(readfile(p4), "foo");
+    remove(",f4");
+}
