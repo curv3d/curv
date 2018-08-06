a0fb8ddd894e00107fb87efc05999a5098c40213
Doug Moen <doug@moens.org>
2018-08-06 17:43:56 -0400

    tweak colour scheme

--- libcurv/ansi_colour.h
+++ libcurv/ansi_colour.h
@@ -13,7 +13,6 @@
 #define AC_BLUE    "\x1b[34m"
 #define AC_MAGENTA "\x1b[35m"
 #define AC_CYAN    "\x1b[36m"
-#define AC_RESET   "\x1b[0m"
 
 #define AC_Black            "\x1b[0;30m"
 #define AC_Blue             "\x1b[0;34m"
@@ -32,9 +31,14 @@
 #define AC_Yellow           "\x1b[1;33m"
 #define AC_White            "\x1b[1;37m"
 
-// Curv colour scheme
+#define AC_RESET    "\x1b[m"
+#define AC_BOLD     "\x1b[1m"
+
+// Curv colour scheme.
+// Instead of the usual "unicorn barf",
+// I want a simple, coordinated colour scheme.
 #define AC_PROMPT   AC_Dark_Grey
-#define AC_MESSAGE  AC_Light_Red
+#define AC_MESSAGE  AC_BOLD
 #define AC_CARET    AC_Light_Red
 
 #endif
