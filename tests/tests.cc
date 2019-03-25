eb6127590f23445dad4b94b01fca3bace7bfb522
Doug Moen <doug@moens.org>
2019-03-25 06:05:45 -0400

    tests: remove tempfiles after tests complete

--- tests/tests.cc
+++ tests/tests.cc
@@ -1,8 +1,13 @@
 #include <gtest/gtest.h>
+#include <libcurv/geom/tempfile.h>
+extern "C" {
+#include <stdlib.h>
+}
 
 int
 main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+    atexit(curv::geom::remove_all_tempfiles);
     return RUN_ALL_TESTS();
 }
