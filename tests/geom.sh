0e61534e572c17a3c127f46ae3e022787c96414a
Doug Moen <doug@moens.org>
2020-06-25 14:55:13 -0400

    unit tests should not depend on user config file

--- tests/geom.sh
+++ tests/geom.sh
@@ -2,8 +2,8 @@ mkdir -p ,gpu
 for f in `ls ../examples/*.curv ../examples/tests/*.curv`; do
   b=$(basename $f .curv)
   echo $f
-  ../debug/curv -o ,gpu/$b.gpu $f || exit 1
+  ../debug/curv -N -o ,gpu/$b.gpu $f || exit 1
   #uncomment the following to regenerate the gpu dir if examples changes
-  #curv -o gpu/$b.gpu $f || exit 1
+  #curv -N -o gpu/$b.gpu $f || exit 1
 done
 diff -r gpu ,gpu || exit 1
