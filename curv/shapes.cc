2ded45406c86ede282c2b4cf9e47677b90e96fe3
Doug Moen <doug@moens.org>
2020-07-15 15:55:35 -0400

    Windows: don't print non-ASCII characters in messages

--- curv/shapes.cc
+++ curv/shapes.cc
@@ -17,10 +17,10 @@ print_shape_generic(
     if (shape.is_2d_ && shape.is_3d_) std::cerr << "/";
     if (shape.is_3d_) std::cerr << "3D";
     std::cerr << " shape "
-        << (shape.bbox_.xmax - shape.bbox_.xmin) << "×"
+        << (shape.bbox_.xmax - shape.bbox_.xmin) << "*"
         << (shape.bbox_.ymax - shape.bbox_.ymin);
     if (shape.is_3d_)
-        std::cerr << "×" << (shape.bbox_.zmax - shape.bbox_.zmin);
+        std::cerr << "*" << (shape.bbox_.zmax - shape.bbox_.zmin);
     std::cerr << "\n";
 }
 
