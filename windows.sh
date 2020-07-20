5ee9e1ae842e152907c5c2f71de51e6b4785def4
Doug Moen <doug@moens.org>
2020-07-20 10:49:12 -0400

    Windows: build script for simplified install

---
+++ windows.sh
@@ -0,0 +1,44 @@
+for pkg in $(cat <<END
+    git
+    make
+    diffutils
+    mingw-w64-x86_64-clang
+    mingw-w64-x86_64-cmake
+    mingw-w64-x86_64-boost
+    mingw-w64-x86_64-mesa
+    mingw-w64-x86_64-openexr
+    mingw-w64-x86_64-intel-tbb
+    mingw-w64-x86_64-glm
+    mingw-w64-x86_64-glew
+    mingw-w64-x86_64-dbus
+    mingw-w64-x86_64-openvdb
+    winpty
+END
+)
+do
+    echo "== installing '$pkg'"
+    echo pacman -S --needed --noconfirm $pkg || exit 1
+done
+
+if test ! -d /curv; then
+    cd /
+    echo == installing Curv source code
+    git clone https://github.com/curv3d/curv || {
+        echo git clone failed
+        rm -r curv
+        exit 1
+    }
+fi
+
+cd /curv || exit 1
+echo == upgrading Curv source code
+make upgrade || exit 1
+
+echo == building Curv executable
+make || exit 1
+
+grep -q 'alias curv=' ~/.bashrc || {
+    echo "== adding 'curv' command to MinGW64 shell"
+    echo 'alias curv="winpty /curv/release/curv"' >> ~/.bashrc
+    source ~/.bashrc
+}
