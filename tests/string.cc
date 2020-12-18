cdef482b28240799fc2e6812d041a9467f8e4cf5
Doug Moen <doug@moens.org>
2020-12-18 15:06:19 -0500

    tests: add some libcurv Symbol tests

--- tests/string.cc
+++ tests/string.cc
@@ -18,11 +18,16 @@ TEST(curv, string)
     ASSERT_EQ(s0->size(), 3u);
     ASSERT_TRUE(strcmp(s0->data(), "foo") == 0);
     ASSERT_STREQ(s0->c_str(), "foo");
+    ASSERT_EQ(s0->type_, Ref_Value::ty_abstract_list);
+    ASSERT_EQ(s0->subtype_, Ref_Value::sty_string);
 
     auto s1 = stringify("sqrt(2)==",sqrt(2));
     ASSERT_STREQ(s1->c_str(), "sqrt(2)==1.4142135623730951");
 
     Symbol_Ref a0 = make_symbol("foo");
+    auto sym0 = a0.to_value().maybe<Symbol>();
+    ASSERT_EQ(sym0->type_, Ref_Value::ty_symbol);
+    ASSERT_EQ(sym0->subtype_, 0);
     Symbol_Ref a1 = make_symbol("foo");
     ASSERT_TRUE(a0 == a1);
     Symbol_Ref a2 = make_symbol("bar");
