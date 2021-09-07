ba039abe7a57df4561bfb0c3b20671a9ef2bd10a
AGD <aloeverdulay@gmail.com>
2021-09-07 23:02:41 +0800

    Updated AppImage build script and workflow

--- build-appimage.sh
+++ build-appimage.sh
@@ -4,29 +4,59 @@ set -x
 set -e
 
 # do cleanup, even if errors occur
-cleanup () {
+cleanup ()
+{
   # remove temp downloaded files and AppImage-build artifacts
   rm -f linuxdeploy-x86_64.AppImage
   rm -f appimagetool-x86_64.AppImage
-  rm -f $desktop_filename
-  rm -f $icon_filename
+  rm -f $desktop_filepath
+  rm -f $icon_filepath
 }
 trap cleanup EXIT
 
-# store repo root as variable
+# utility function - rename directory
+shopt -s dotglob
+rnmdir ()
+{
+  local from=$1
+  local to=$2
+
+  if [[ -d "$from" ]]; then
+    if [[ $(ls -A "$from") ]]; then
+      for item in "$from"/*; do
+        rnmdir "$item" "$to/$(basename "$item")"
+      done
+      [[ -d "$from" ]] && rm -rf "$from"
+    else
+      mkdir -p "$to"
+      rmdir "$from"
+    fi
+  elif [[ -f "$from" ]]; then
+    mkdir -p "$(dirname "$to")"
+    mv -u "$from" "$to"
+  fi
+}
+
+# variables
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 BUILD_DIR=release
 APPDIR=$BUILD_DIR/AppDir
+HICOLOR_ICONS_PATH=$APPDIR/usr/share/icons/hicolor
+ID=org.curv3d.curv # software id
 
 cd "$REPO_ROOT"
+
+# cleanup leftover artifacts in AppDir
+rm -f $APPDIR/*.png
+rm -f $APPDIR/*.svg
+rm -f $APPDIR/*.desktop
 
 # build project and install files into AppDir/usr
 make -j$(nproc)
 make install DESTDIR=AppDir
 
 # rename usr/local -> usr/
-cd $APPDIR/usr/local && mv * .. && cd .. && rmdir local
-cd "$REPO_ROOT"
+rnmdir $APPDIR/usr/local $APPDIR/usr
 
 # patch absolute paths (make binary relocatable)
 sed -i -e 's#/usr#././#g' $APPDIR/usr/bin/curv
@@ -67,9 +97,13 @@ EOL
   sleep 1
 fi
 
-# create AppImage's icon using curv
-icon_filename=$BUILD_DIR/curv.png
-./$APPDIR/usr/bin/curv -o $icon_filename -O xsize=512 -O ysize=512 icon.curv
+# create AppImage's icons using curv
+icon_filename=curv.png
+icon_filepath=$BUILD_DIR/$icon_filename
+./$APPDIR/usr/bin/curv -o $BUILD_DIR/$icon_filename -O xsize=512 -O ysize=512 icon.curv
+./$APPDIR/usr/bin/curv -o $HICOLOR_ICONS_PATH/256x256/apps/$icon_filename -O xsize=256 -O ysize=256 icon.curv
+./$APPDIR/usr/bin/curv -o $HICOLOR_ICONS_PATH/128x128/apps/$icon_filename -O xsize=128 -O ysize=128 icon.curv
+./$APPDIR/usr/bin/curv -o $HICOLOR_ICONS_PATH/64x64/apps/$icon_filename -O xsize=64 -O ysize=64 icon.curv
 
 # kill dummy X server
 if [[ ! -z $dummy_xconf ]]; then
@@ -78,15 +112,14 @@ if [[ ! -z $dummy_xconf ]]; then
 fi
 
 # create desktop entry for AppImage
-desktop_filename=$BUILD_DIR/curv.desktop
-cat > $desktop_filename << EOL
+desktop_filepath=$BUILD_DIR/$ID.desktop
+cat > $desktop_filepath << EOL
 [Desktop Entry]
 Name=curv
 GenericName=
 Exec=./AppRun %U
 Type=Application
 StartupNotify=true
-Path=
 Icon=curv
 StartupWMClass=curv
 Categories=Graphics
@@ -94,22 +127,25 @@ Terminal=true
 Comment=Art creator app using mathematics
 EOL
 
-# now, build AppImage using linuxdeploy and appimagetool
-# download linuxdeploy and appimagetool
+# add metainfo to AppDir
+rm -rf $APPDIR/usr/share/metainfo
+mkdir -p $APPDIR/usr/share/metainfo
+cp metainfo.xml $APPDIR/usr/share/metainfo/$ID.appdata.xml
+
+# now, build AppImage using linuxdeploy
+# download linuxdeploy
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
-
-# make them executable
 chmod +x linuxdeploy-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage
 
-# bundle shared libraries for curv to AppDir, and build AppImage from AppDir
+# bundle shared libraries for curv to AppDir and build AppImage from AppDir
+export UPDATE_INFORMATION="gh-releases-zsync|curv3d|curv|latest|Curv-*x86_64.AppImage.zsync"
 ./linuxdeploy-x86_64.AppImage \
-  --appdir $APPDIR \
-  --custom-apprun AppRun \
-  -d $desktop_filename \
-  -i $icon_filename
-./appimagetool-x86_64.AppImage $APPDIR
+  --appdir=$APPDIR \
+  --custom-apprun=AppRun \
+  --desktop-file=$desktop_filepath \
+  --icon-file=$icon_filepath \
+  --output=appimage
 
 # move built AppImage into release folder
-mv curv*.AppImage $BUILD_DIR
+mv Curv-*.AppImage $BUILD_DIR
+mv Curv-*.AppImage.zsync $BUILD_DIR
