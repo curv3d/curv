a962fd804b8c0cb24f9e2dee78b2b6c8e3ea8d8b
Doug Moen <doug@moens.org>
2021-10-25 17:34:43 -0400

    eliminate rendering holes from SVX->STL output
    Added a margin around the perimeter of the voxel grid so that the model
    doesn't touch the faces of the grid.

--- tools/svx.sh
+++ tools/svx.sh
@@ -2,6 +2,10 @@
 #
 # Usage: svx.sh [curv-source-file] [vsize]
 # 
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 curv-source-file vsize"
+    exit 1
+fi
 
 FILE=$1
 VSIZE=$2
@@ -9,27 +13,29 @@ OUTDIR=${3:-"density"}
 
 FDUR=0.01
 
-DIMENSIONS=$(curv -x "file \"$FILE\" >> size")
+MARGIN=2
+DIMENSIONS=$(curv -x "size(file \"$FILE\") + 2*$MARGIN*$VSIZE")
 WIDTH=$(echo "$DIMENSIONS"  | jq '.[0]')
 HEIGHT=$(echo "$DIMENSIONS" | jq '.[1]')
 DEPTH=$(echo "$DIMENSIONS"  | jq '.[2]')
-TOTAL_DURATION=$(echo "scale=10 ; ($DEPTH / $VSIZE) * $FDUR" | bc)
+XSIZE=$(curv -x "ceil($WIDTH  / $VSIZE)")
+YSIZE=$(curv -x "ceil($HEIGHT / $VSIZE)")
+ZSIZE=$(curv -x "ceil($DEPTH  / $VSIZE)")
+echo "shape size=$DIMENSIONS grid=[$XSIZE,$YSIZE,$ZSIZE]"
 
 BASENAME="slice"
 DIRNAME=$(dirname "$FILE")
 OUTPUT="$BASENAME"'-*';
 
-XSIZE=$(echo "$WIDTH  / $VSIZE" | bc )
-YSIZE=$(echo "$HEIGHT / $VSIZE" | bc )
-ZSIZE=$(echo "$DEPTH  / $VSIZE" | bc )
-VSIZE_METERS=$(echo "scale=10 ; $VSIZE / 1000" | bc )
+VSIZE_METERS=$(curv -x "$VSIZE / 1000")
 
 cd "$DIRNAME"
 
 curv                         \
 -o "$OUTPUT.png"             \
 -O taa=6                     \
--O animate="$TOTAL_DURATION" \
+-O aa=6                      \
+-O animate="$FDUR*$ZSIZE"    \
 -O fdur=$FDUR                \
 -O xsize=$XSIZE              \
 -O ysize=$XSIZE              \
@@ -38,20 +44,23 @@ curv                         \
   {vsize,shape} ->
   let
     fdur = $FDUR;
-    zmin = shape.bbox.[MIN,Z];
+    zmin = shape.bbox.[MIN,Z] - $MARGIN*vsize;
   in make_shape {
-    dist: [x,y,z,t] ->
+    dist: [x,y,_,t] ->
       let
         frameno = floor(t / fdur);
-        zz = zmin + vsize*frameno;
-      in shape.dist[x,y,zz,0];
+        z = zmin + vsize*frameno;
+      in shape.dist[x,y,z,0];
     colour: p -> white;
     is_2d: true;
-    bbox: shape.bbox;
+    bbox: [shape.bbox.[MIN] - $MARGIN*vsize, shape.bbox.[MAX] + $MARGIN*vsize];
     render: {bg: black};
   }
 )
 { shape=file \"$(basename $FILE)\", vsize=$VSIZE }";
+
+n=$(ls | grep '^slice-0*\.png$' | sed -e 's/^slice-//' -e 's/\.png$//' | wc -c)
+n=$(expr $n - 1)
 
 rm -r "$OUTDIR" &> /dev/null
 mkdir "$OUTDIR"
@@ -67,7 +76,7 @@ echo "<?xml version=\"1.0\"?>
    voxelSize=\"$VSIZE_METERS\" subvoxelBits=\"8\" slicesOrientation=\"Z\" >
 
     <channels>
-        <channel type=\"DENSITY\" bits=\"8\" slices=\"$OUTDIR/$BASENAME-%03d.png\" />
+        <channel type=\"DENSITY\" bits=\"8\" slices=\"$OUTDIR/$BASENAME-%0${n}d.png\" />
     </channels>
 
     <metadata>
@@ -77,6 +86,6 @@ echo "<?xml version=\"1.0\"?>
 </grid>" > manifest.xml;
 
 ZIP_FILE=$(basename "$FILE" .curv).svx
-rm "$ZIP_FILE"
+rm -f "$ZIP_FILE"
 zip -r "$ZIP_FILE" manifest.xml "$OUTDIR"/
 rm -r manifest.xml "$OUTDIR"
